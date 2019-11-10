b8f2b23225aa1458cfcab2c784c6975756055385
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-11-11 00:42:32 +0330

    Fix the input issue

--- lib/screens/quick_sort/quick_sort_body.dart
+++ lib/screens/quick_sort/quick_sort_body.dart
@@ -38,7 +38,7 @@ class _QuickSortBodyState extends State<QuickSortBody> {
               border:
                   OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
               labelText: 'Input',
-              hintText: 'Enter your number here and then click Add',
+              hintText: 'Enter your number here',
             ),
             keyboardType: TextInputType.number,
             inputFormatters: <TextInputFormatter>[
@@ -55,33 +55,51 @@ class _QuickSortBodyState extends State<QuickSortBody> {
 
           SizedBox(height: 10.0),
 
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-                side: BorderSide(color: Colors.white)),
-            child: Container(
-              child: Text('Add'),
-            ),
-            onPressed: () {
-              // inputNumbers = inputController.text;
-            },
+          // Buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Remove button
+              FlatButton(
+                child: Text('Clear list'),
+                onPressed: () {
+                  _clreattoList();
+                },
+              ),
+
+              // Add button
+              FlatButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Colors.white)),
+                child: Container(
+                  child: Text('ADD'),
+                ),
+                onPressed: () {
+                  _addToList(inputController.text);
+                },
+              ),
+            ],
           ),
 
           SizedBox(height: 20.0),
 
           // Show inputed numbers
           Container(
-            padding: const EdgeInsets.all(15.0),
-            height: 50.0,
+            padding: const EdgeInsets.all(18.0),
+            height: 60.0,
             width: width,
             decoration: BoxDecoration(
                 border: Border.all(width: 1.0),
                 borderRadius: BorderRadius.all(Radius.circular(10.0))),
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Text('Unsorted: ' + inputController.text.toString()),
+              child: Text(
+                'Unsorted: ' + numbersList.toString(),
+                style: TextStyle(fontSize: 16.0),
+              ),
             ),
           ),
 
@@ -93,11 +111,15 @@ class _QuickSortBodyState extends State<QuickSortBody> {
             height: 50.0,
             width: width,
             decoration: BoxDecoration(
+                color: Colors.blue,
                 border: Border.all(width: 1.0),
                 borderRadius: BorderRadius.all(Radius.circular(10.0))),
             child: SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Text('Sorted: ' + intNumberList.toString()),
+              child: Text(
+                'Sorted: ' + intNumberList.toString(),
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ],
@@ -105,12 +127,19 @@ class _QuickSortBodyState extends State<QuickSortBody> {
     );
   }
 
+// Add element to list
   void _addToList(String userNumber) {
     setState(() {
       // numbersList.add(userNumber);
       intNumberList = int.tryParse(userNumber);
       numbersList.add(intNumberList);
-      print(intNumberList);
+    });
+  }
+
+// Clear all list elements
+  void _clreattoList() {
+    setState(() {
+      numbersList.clear();
     });
   }
 }
