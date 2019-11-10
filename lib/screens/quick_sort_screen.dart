7a990c6989f4fb85b9854c7a3522b405d45347c1
Yousef Roshandel <Yousef.r1999@gmail.com>
2019-11-10 20:31:52 +0330

    Create QuickSort about class

--- lib/screens/quick_sort_screen.dart
+++ lib/screens/quick_sort_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:sasa_app/screens/quick_sort_about.dart';
 
 class QuickSortScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return new Scaffold(
-      appBar: new AppBar(
-        title: Text('QuickSort'),
-        centerTitle: true,
-      ),
-      body: new Center(
-        child: Text('data'),
-      ),
-    );
+        appBar: new AppBar(
+          title: Text('QuickSort'),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: new SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              new QuickSortAbout(),
+              new Container(),
+            ],
+          ),
+        ));
   }
 }
